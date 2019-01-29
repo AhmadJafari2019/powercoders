9dd7ca7682653002410e38abd5d0e35654252b59
Ahmad <ahmad.hazara@gmx.ch>
2019-01-29 16:50:29 +0100

    - Add Quantity Input in html- - Set the keyup event for that in js file-

--- Shopping/shopping.js
+++ Shopping/shopping.js
@@ -1,9 +1,11 @@
 function domContentLoaded() {
   /** Widget that the User types an item in to */
   const inputBox = document.getElementById('item');
+  const quantityBox = document.getElementById('quantity');
   const shoppingList = document.querySelector('ul');
   const addItemButton = document.querySelector('button');
   const clearButton = document.getElementById('clear');
+
 
   clearButton.addEventListener('click', function (event) {
     const listItem = document.querySelectorAll('li');
@@ -32,12 +34,14 @@ function domContentLoaded() {
       alert('koshad in khalie');
       return;
     }
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityBox.value.trim()));
     inputBox.value = '';
+    quantityBox.value = '';
     addItemButton.disabled = true;
     clearButton.disabled = false;
     inputBox.focus();
   });
+
 
   inputBox.addEventListener('keyup', function (event) {
     const trimmedValue = inputBox.value.trim();
@@ -50,62 +54,70 @@ function domContentLoaded() {
     if (event.key !== 'Enter') {
       return;
     }
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityBox.value.trim()));
     inputBox.value = '';
     addItemButton.disabled = true;
     clearButton.disabled = false;
-
   });
 
   inputBox.focus();
 
 }
 
-if (document.readyState === 'loading'){
+if (document.readyState === 'loading') {
   //DOMContentLoaded has not fired yet.
   document.addEventListener('DOMContentLoaded', function () {
     domContentLoaded();
   });
-} else{
+} else {
   //DOMContentLoaded has fired.
   domContentLoaded();
 }
 
 
+/**
+ * Create and return an 'li' element for inculusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @param {string} quantity
+ * @return {HTMLElement} li element
+ */
+function createNewListItem(itemName, quantity) {
+  const listItem = document.createElement('li');
 
-  /**
-   * Create and return an 'li' element for inculusion in the shopping list.
-   *
-   * @param {string} itemName Name of the item to add to the list
-   * @return {HTMLElement} li element
-   */
+  const listText = document.createElement('span');
+  listText.textContent = itemName;
 
-  function elseTest(num) {
-    if (num < 10) {
-      console.log('less than 10');
-    } else if (num > 10) {
-      console.log('bigger than 10');
-    }
+
+  const deleteButton = document.createElement('i');
+  listItem.appendChild(deleteButton).className = 'fa fa-trash';
+
+  deleteButton.addEventListener('click', function (event) {
+    console.log('Delete is clicked');
+    listItem.remove();
+    const inputBox = document.getElementById('item');
+    inputBox.focus();
+  });
+
+  listItem.appendChild(listText);
+
+  if (quantity !== '') {
+    listItem.appendChild(document.createTextNode(' '));
+    const span = document.createElement('span');
+    span.textContent = `(${quantity})`;   // '(' + quantity + ')'
+    listItem.appendChild(span);
   }
 
-  function createNewListItem(itemName) {
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    listText.textContent = itemName;
+  listItem.appendChild(deleteButton);
+  return listItem;
+}
 
-    const deleteButton = document.createElement('i');
-    listItem.appendChild(deleteButton).className = 'fa fa-trash';
 
-    deleteButton.addEventListener('click', function (event) {
-      console.log('Delete is clicked');
-      listItem.remove();
-      const inputBox = document.getElementById('item');
-      inputBox.focus();
-    });
-
-    listItem.appendChild(listText);
-    listItem.appendChild(deleteButton);
-    return listItem;
+function elseTest(num) {
+  if (num < 10) {
+    console.log('less than 10');
+  } else if (num > 10) {
+    console.log('bigger than 10');
   }
-
+}
 
